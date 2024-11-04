e8d1c14e84a8d6fb846604c5c76b8caf3c45484b
Alii <tigerli808@live.com>
2024-11-03 16:45:18 -1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import math
 from pathlib import Path
-import mysql.connector 
+import mysql
 # -----------------------------------------------------------------------------
 #
 
